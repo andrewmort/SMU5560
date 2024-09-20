2c25a123ea82903d6e61ba5edea27d42a42bad3f
Andrew Mort <andrewnmort@gmail.com>
2024-09-19 22:13:52 -0400

    update libraries

---
+++ arduino/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on
+  delay(1000);                       // Wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // Turn the LED off
+  delay(1000);                       // Wait for a second
+}
